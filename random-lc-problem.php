8fa0497e90fa4c9bec0689e36cb28b2d93fc18ee
Al Amin <hmalaminmb4@gmail.com>
2025-05-21 22:34:48 +0600

    LC If string is prefix of concat words

--- random-lc-problem.php
+++ random-lc-problem.php
@@ -22,3 +22,31 @@ function longestprefix( $str ) {
 }
 
 print_r( longestprefix( ['shopify', 'Shopping', 'shopcart'] ) );
+
+// Problem: Check if string is prefix of concatenated words
+// 1961. Check If String Is a Prefix of Array
+
+// Given a string s and an array of strings words, 
+// determine whether s is a prefix string of words.
+// A string s is a prefix string of words if s can be made by concatenating 
+// the first k strings in words for some positive k no larger than words.length.
+// Return true if s is a prefix string of words, or false otherwise.
+function isPrefixString($s, $words) {
+    $str = '';
+    
+    for( $i = 0; $i < count( $words ); $i++ ) {
+        $str .= $words[$i];
+        
+        if( $str === $s ) {
+            return true;
+            break;
+        }
+    }
+    
+    return false;
+}
+
+$s = "a";
+$words = ["aa","aaaa","banana"];
+
+var_dump( isPrefixString($s, $words)  );
