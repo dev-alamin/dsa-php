50d06fc8da7d4837044666e4339d327b81082942
Al Amin <hmalaminmb4@gmail.com>
2025-05-26 13:01:21 +0600

    New: 643: Max avearge subarray 1

--- random-lc-problem.php
+++ random-lc-problem.php
@@ -229,3 +229,36 @@ function maxSumSubarrayOfSizeK($arr, $k) {
 $arr = [2, 1, 5, 1, 3, 2];
 $k = 3;
 echo maxSumSubarrayOfSizeK($arr, $k); // Output: 9
+
+// 3364. Minimum Positive Sum Subarray 
+// Level: Easy, Topics: Sliding Window, Prefix Sum
+function findMaxAverage($arr, $k) {
+    $maxSum = PHP_INT_MIN;
+    $windowSum = 0;
+    $start = 0;
+
+    for ($end = 0; $end < count($arr); $end++) {
+        $windowSum += $arr[$end];
+
+        if ($end >= $k - 1) {
+            $maxSum = max($maxSum, $windowSum);
+            $windowSum -= $arr[$start];
+            $start++;
+        }
+    }
+
+    return $maxSum / $k;
+}
+
+
+// Test
+$nums = [8860,-853,6534,4477,-4589,8646,-6155,-5577,-1656,-5779,
+-2619,-8604,-1358,-8009,4983,7063,3104,-1560,4080,2763,5616,-2375,
+2848,1394,-7173,-5225,-8244,-809,8025,-4072,-4391,-9579,1407,6700,2421,
+-6685,5481,-1732,-8892,-6645,3077,3287,-4149,8701,-4393,-9070,-1777,2237,
+-3253,-506,-4931,-7366,-8132,5406,-6300,-275,-1908,67,3569,1433,-7262,-437,
+8303,4498,-379,3054,-6285,4203,6908,4433,3077,2288,9733,-8067,3007,9725,9669,
+1362,-2561,-4225,5442,-9006,-429,160,-9234,-4444,3586,-5711,-9506,-79,-4418,
+-4348,-5891];
+$k = 93;
+echo findMaxAverage($nums, $k);
